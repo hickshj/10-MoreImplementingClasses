bb20fa0c417eef32faaa16ce9106b27ad0fadfd7
hickshj <hickshj@rose-hulman.edu>
2018-09-27 11:26:03 -0400

    Liebestraum

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -37,62 +37,81 @@ def main():
         for j in range(4):
             mckinley.hour_passes()
 
+    # ----------------------------------------------------------------------
+    # Done: 2. Implement a class called   Baby   that has a constructor and
+    #   two methods, as described below.  Your finished Baby class should
+    #   cause the code above to display the expected output.  Hint: Your
+    #   class will need instance variables that you must figure out.
+    #
+    # Constructor method
+    #     What comes in:
+    #        -- self
+    #        -- a string for the name of the baby
+    #     What goes out:  Nothing (i.e., None).
+    #     Side effects:
+    #        -- Sets instance variables as needed
+    #        -- Prints 'Hello baby <your baby's name>!'
+    #
+    # feed_baby
+    #     What comes in:
+    #        -- self
+    #     What goes out:  Nothing (i.e., None).
+    #     Side effects:
+    #        -- Prints 'Thank you for feeding baby <your baby's name>.'
+    #        -- Modifies instance variables if needed
+    #
+    # hour_passes
+    #     What comes in:
+    #        -- self
+    #     What goes out:  Nothing (i.e., None).
+    #     Side effects:
+    #      -- If it is the first time this function has been called
+    #           since Baby was created or fed:
+    # 	          -- Prints 'Baby <your baby's name> is sleeping.'
+    #      -- If it is the second time this function has been called
+    #           since baby was created or fed:
+    # 	         -- Prints 'Baby <your baby's name> is awake.  Time for food.'
+    #      -- If it is the third (or more) time this function has been called
+    #           since baby was created or fed:
+    # 	         -- Prints:
+    #            'Baby <your baby's name> is CRYING uncontrollably!  Feed the Baby!'
+    #
+    # After implementing this class run this module and compare your output
+    #   to the expected output of main.
+    #
+    # Notice that the baby is never printed, so you are not required to make
+    #     a __repr__ method.  Each method call above simply does a print as
+    #     a side effect.
+    # ----------------------------------------------------------------------
 
-# ----------------------------------------------------------------------
-# TODO: 2. Implement a class called   Baby   that has a constructor and
-#   two methods, as described below.  Your finished Baby class should
-#   cause the code above to display the expected output.  Hint: Your
-#   class will need instance variables that you must figure out.
-#
-# Constructor method
-#     What comes in:
-#        -- self
-#        -- a string for the name of the baby
-#     What goes out:  Nothing (i.e., None).
-#     Side effects:
-#        -- Sets instance variables as needed
-#        -- Prints 'Hello baby <your baby's name>!'
-#
-# feed_baby
-#     What comes in:
-#        -- self
-#     What goes out:  Nothing (i.e., None).
-#     Side effects:
-#        -- Prints 'Thank you for feeding baby <your baby's name>.'
-#        -- Modifies instance variables if needed
-#
-# hour_passes
-#     What comes in:
-#        -- self
-#     What goes out:  Nothing (i.e., None).
-#     Side effects:
-#      -- If it is the first time this function has been called
-#           since Baby was created or fed:
-# 	          -- Prints 'Baby <your baby's name> is sleeping.'
-#      -- If it is the second time this function has been called
-#           since baby was created or fed:
-# 	         -- Prints 'Baby <your baby's name> is awake.  Time for food.'
-#      -- If it is the third (or more) time this function has been called
-#           since baby was created or fed:
-# 	         -- Prints:
-#            'Baby <your baby's name> is CRYING uncontrollably!  Feed the Baby!'
-#
-# After implementing this class run this module and compare your output
-#   to the expected output of main.
-#
-# Notice that the baby is never printed, so you are not required to make
-#     a __repr__ method.  Each method call above simply does a print as
-#     a side effect.
-# ----------------------------------------------------------------------
+    ########################################################################
+    # The   Baby   class (and its methods) should begins here.
+    # Here is a reminder for the syntax to create a new class.
+    #
+    #      class NameOfClass(object):
+    #          """ Brief description of what objects of the class 'are'. """
+    #
+    ########################################################################
 
-########################################################################
-# The   Baby   class (and its methods) should begins here.
-# Here is a reminder for the syntax to create a new class.
-#
-#      class NameOfClass(object):
-#          """ Brief description of what objects of the class 'are'. """
-#
-########################################################################
+
+class Baby(object):
+    def __init__(self, name):
+        self.name = name
+        self.hours_passed_without_feeding = 0
+        print("Hello baby {}!".format(name))
+
+    def feed_baby(self):
+        print("Thank you for feeding baby {}.".format(self.name))
+        self.hours_passed_without_feeding = 0
+
+    def hour_passes(self):
+        self.hours_passed_without_feeding = self.hours_passed_without_feeding + 1
+        if self.hours_passed_without_feeding == 1:
+            print("Baby {} is sleeping.".format(self.name))
+        elif self.hours_passed_without_feeding == 2:
+            print("Baby {} is awake.  Time for food.".format(self.name))
+        else:
+            print("Baby {} is CRYING uncontrollably! Feed the Baby!".format(self.name))
 
 
 # ----------------------------------------------------------------------
